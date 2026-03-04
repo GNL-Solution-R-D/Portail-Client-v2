d18753662d010330f5bc1f44ec597a742512b1a5
Nuksf <69607809+Nuksf@users.noreply.github.com>
2026-03-04 22:36:57 +0100

    Update config_loader.php

--- config_loader.php
+++ config_loader.php
@@ -3,6 +3,6 @@ $configFile = __DIR__ . '/config.php';
 if (file_exists($configFile)) {
     require_once $configFile;
 } else {
-    require_once __DIR__ . '/config.example.php';
+    require_once __DIR__ . '/config.php';
 }
 ?>
