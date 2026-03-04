0a46e54b8cacfebf19186215944644129bbab9ef
Nuksf <69607809+Nuksf@users.noreply.github.com>
2026-03-05 00:59:48 +0100

    Update header.php

--- include/header.php
+++ include/header.php
@@ -1,7 +1,7 @@
 <!-- Bandeau sup�rieur plein largeur -->
 <div hidden="">
-    <!--$--><!--/$-->
-  </div>
+  <!--$--><!--/$-->
+</div>
   <script>((e,t,r,o,a,n,s,i)=>{let l=document.documentElement,d=["light","dark"];function c(t){var r;(Array.isArray(e)?e:[e]).forEach(e=>{let r="class"===e,o=r&&n?a.map(e=>n[e]||e):a;r?(l.classList.remove(...o),l.classList.add(n&&n[t]?n[t]:t)):l.setAttribute(e,t)}),r=t,i&&d.includes(r)&&(l.style.colorScheme=r)}if(o)c(o);else try{let e=localStorage.getItem(t)||r,o=s&&"system"===e?window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light":e;c(o)}catch(e){}})("class","theme","system",null,["light","dark"],null,true,true)</script>
   <div class="bg-background w-full">
     <nav class="w-full overflow-visible rounded-lg border border-transparent p-2 shadow-transparent">
@@ -14,7 +14,7 @@
             <a href="#" class="hover:text-primary dark:hover:text-primary inline-flex items-center gap-1.5 text-sm transition-colors duration-300 ease-in">Projects</a>
           </nav>
           <p class="mt-1 ml-1 text-base font-semibold">GNL Solution</p>
-          <p class="mt-1 ml-1 text-base font-semibold">Portail Association &amp; Micro Entreprise</p>
+          <p class="mt-1 ml-1 text-base font-semibold">Portail Association &amp; Entreprise</p>
         </div>
         <div class="ml-auto flex items-center gap-2">
           <div class="relative hidden w-full max-w-sm min-w-[200px] items-center md:block">
