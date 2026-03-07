2e05d23f396dd903634bf4e1aa023b37c7e3008e
Nuksf <69607809+Nuksf@users.noreply.github.com>
2026-03-07 20:06:27 +0100

    Update connexion.php

--- pages/connexion.php
+++ pages/connexion.php
@@ -107,14 +107,14 @@ require_once '../include/csrf.php';
   <div class="space-y-2">
     <label data-slot="label"
            class="flex items-center gap-2 select-none text-sm font-semibold"
-           for="email">
+           for="siret">
       SIRET
     </label>
     <input type="text"
            required 
            data-slot="input"
            class="file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive h-11"
-           id="email"
+           id="siret"
            name="username"
            placeholder="00000000000000" />
   </div>
@@ -131,7 +131,7 @@ require_once '../include/csrf.php';
            class="file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive h-11"
            id="username"
            name="username"
-           placeholder="00000000000000" />
+           placeholder="name" />
   </div>
 
   <div class="space-y-2">
