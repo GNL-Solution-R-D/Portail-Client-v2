8f9a5bfdcdb444199e95ff2c18f3bfce38b2ae43
Nuksf <69607809+Nuksf@users.noreply.github.com>
2026-03-07 23:23:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,21 +7,8 @@
     <title>Connexion - GNLSolution</title>
     <link rel="stylesheet" href="assets/styles/login.css">
 </head>
-<body>
-    <div class="container">
-        <div class="login-box">
-            <h1>GNL SOLUTION</h1>
-            <p>Portail Association & Entreprise</p>
-            <form action="login.php" method="POST">
-                <input type="text" name="username" placeholder="NRA ou Siret" required>
-                <input type="password" name="password" placeholder="Mot de Passe" required>
-                <button type="submit">Connexion</button>
-            </form>
-            <a href="activation.php">Premiere Connexion ?</a>
-            <a href="disable_u2f.php">Perte de la cle ?</a>
-        </div>
-        <div class="image-box">
-        </div>
-    </div>
+<?php 
+
+    header('Location: /maintenance'); ?>
 </body>
 </html>
