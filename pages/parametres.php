5b86f482f5dd0c1f243ff1722792f7868de4ceaf
Nuksf <69607809+Nuksf@users.noreply.github.com>
2026-03-09 20:10:40 +0100

    Update parametres.php

--- pages/parametres.php
+++ pages/parametres.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['user'])) {
 }
 
 require_once '../config_loader.php';
-include_once '../assets/lang.php';
+include_once '../include/lang.php';
 
 if (empty($_SESSION['settings_csrf_token'])) {
     $_SESSION['settings_csrf_token'] = bin2hex(random_bytes(32));
